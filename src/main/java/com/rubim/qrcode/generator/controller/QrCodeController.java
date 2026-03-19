4ec832b056bb43f5fd5e804be66f889f44c51224
Matheus Rubim <matheus.rubim0408@gmail.com>
2026-03-19 12:50:16 -0300

    feat: enable CORS for all origins

--- src/main/java/com/rubim/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/rubim/qrcode/generator/controller/QrCodeController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/qrcode")
+@CrossOrigin(origins = "*")
 public class QrCodeController {
 
     private final QrCodeGeneratorService qrCodeGeneratorService;
